01ba35f97c5a7ca3acf8b8bd4206e0ec7b5f14ed
Larissa Bedin <larissa.bedin@abensoft.com.br>
2021-11-03 10:41:47 -0300

    att funcionario validate

--- support/form_ctrl_fun.erl
+++ support/form_ctrl_fun.erl
@@ -10,22 +10,26 @@
 -author("abensoft").
 -include_lib("zotonic.hrl").
 %% API
--export([event/2]).
+-export([event/2,
+        fields/1]).
+
+fields(Args1)->
+    Cpf = proplists:get_value("cpf", Args1),
+    Nome = proplists:get_value("nome", Args1),
+    Ativo = proplists:get_value("ativo", Args1),
+    Ativo1 = case Ativo == "on" of
+                 true -> 1;
+                 _ -> 0
+             end,
+    Setor = list_to_integer(proplists:get_value("setor_id", Args1)),
+    {Cpf, Nome, Ativo1, Setor}.
 
 event(#submit{message ={add_funcionario, Args}}, Context) ->
 
     ?DEBUG(Args),
     ?DEBUG(z_context:get_q_all(Context)),
     Args1 = lists:merge(Args, z_context:get_q_all(Context)),
-
-    Cpf = proplists:get_value("cpf", Args1),
-    Nome = proplists:get_value("nome", Args1),
-    Ativo = proplists:get_value("ativo", Args1),
-    Ativo1 = case Ativo == "on" of
-        true -> 1;
-          _ -> 0
-    end,
-    Setor = list_to_integer(proplists:get_value("setor_id", Args1)),
+    {Cpf, Nome, Ativo1, Setor}=form_ctrl_fun:fields(Args1),
     {ok, CatId} = m_category:name_to_id(person, Context),
     {ok, RscId} = m_rsc:insert([{title, Nome}, {category_id, CatId }], Context),
     m_funcionario:insert(Cpf, Nome, Ativo1, Setor, RscId, Context),
@@ -36,15 +40,7 @@ event(#submit{message = {edit_funcionario, Args}}, Context) ->
     Args1 = lists:merge(Args, z_context:get_q_all(Context)),
     ?DEBUG(Args1),
     Id = z_utils:depickle(proplists:get_value("id", Args1), Context),
-    Cpf = proplists:get_value("cpf", Args1),
-    Nome = proplists:get_value("nome", Args1),
-    Ativo = proplists:get_value("ativo", Args1),
-    ?DEBUG(Ativo),
-    Ativo1 = case Ativo == "on" of
-        true ->  1;
-        _ -> 0
-    end,
-    Setor = form_utils:to_integer(proplists:get_value("setor_id", Args1)),
+    {Cpf, Nome, Ativo1, Setor}=form_ctrl_fun:fields(Args1),
     RscId = z_utils:depickle(proplists:get_value("rsc_id", Args1), Context),
     m_rsc:update(RscId, [{title, Nome}], Context),
     m_funcionario:update(Id, Cpf, Nome, Ativo1, Setor,  Context),
